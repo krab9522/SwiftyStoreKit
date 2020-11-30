02071748a6b8f237e4d052a6c7bade7516011a0f
Artem Kirnos <>
2020-11-30 14:32:43 +0600

    remove comments

--- Sources/SwiftyStoreKit/SwiftyStoreKit.swift
+++ Sources/SwiftyStoreKit/SwiftyStoreKit.swift
@@ -308,13 +308,7 @@ extension SwiftyStoreKit {
         
         return InAppReceipt.getDistinctPurchaseIds(ofType: type, inReceipt: receipt)
     }
-    
-    public class func showPresentCodeRedemptionSheet() {
-        if #available(iOSApplicationExtension 14.0, *) {
-            sharedInstance.skPaymentQueue.presentCodeRedemptionSheet()
-        }
-    }
-    
+        
     public class var paymentQueue: SKPaymentQueue {
         sharedInstance.skPaymentQueue
     }
